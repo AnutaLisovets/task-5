91cb693fb607c6a54860de61e619ca04f3f34ff2
Anna <anutalisovec@gmail.com>
2022-05-16 18:47:24 +0300

    Update Logic.cpp
    Troubleshooting

--- task 5/Logic.cpp
+++ task 5/Logic.cpp
@@ -1,12 +1,19 @@
 #include "Header.h"
 
-void reversElements(int* array, int size) {
+void reversElements(int *array, int size);
+int calculateNoZeroElements(int *array, int size);
+int calculateCountOfNumbersBiggerThenN(int *array, int size);
+int calculateCountOfNumbers(int *array, int size);
+bool checkElements(int *array, int size);
+void bubbleSort(int *array, int size);
+
+void reversElements(int *array, int size) {
 	for (int i = size - 1; i != -1; i--) {
 		cout << array[i] << " ";
 	}
 }
 
-int calculateNoZeroElements(int* array, int size) {
+int calculateNoZeroElements(int *array, int size) {
 	int count = 0;
 	for (int i = 0; i < size; i++) {
 		if (array[i] != 0) {
@@ -16,13 +23,13 @@ int calculateNoZeroElements(int* array, int size) {
 	return count;
 }
 
-int calculateCountOfNumbersBiggerThenN(int* array, int size) {
+int calculateCountOfNumbersBiggerThenN(int *array, int size) {
 	int number;
 	int count = 0;
 	cout << "input number: ";
 	cin >> number;
 
-	for (int i = 0;i < size; i++) {
+	for (int i = 0; i < size; i++) {
 		if (array[i] > number) {
 			count++;
 		}
@@ -31,30 +38,34 @@ int calculateCountOfNumbersBiggerThenN(int* array, int size) {
 	/*cout << "this massive have " << count << " numbers which bigger than " << number;*/
 }
 
-int calculateCountOfNumbers(int* array, int size) {
+int calculateCountOfNumbers(int *array, int size) {
 	int sum = 0;
 	int count = 0;
-	for (int i = 0;i < size;i++) {
+	for (int i = 0; i < size; i++) {
 		sum += array[i];
 	}
 	int avg = sum / size;
-	for (int i = 0;i < size; i++) {
-                if(array[i]>avg)
-		count++;
+	for (int i = 0; i < size; i++) {
+		if (array[i] > avg) {
+			count++;
+		}
 	}
+	return count;
 }
 
-bool checkElements(int* array, int size) {
-	for (int i = 0;i < size; i++) {
-		if (array[i] = array[i + 1])
-			return false;
+bool checkElements(int *array, int size) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (array[i] == array[j] && i != j)
+				return false;
+		}
 	}
 	return true;
 }
 
-void bubbleSort(int* array, int size) {
-	for (int j = 0;j < size - 1;j++) {
-		for (int i = 0;i < size - 1 - j;i++) {
+void bubbleSort(int *array, int size) {
+	for (int j = 0; j < size - 1; j++) {
+		for (int i = 0; i < size - 1 - j; i++) {
 			if (array[i] > array[i + 1]) {
 				int temp = array[i];
 				array[i] = array[i + 1];
--- task 5/inout.cpp
+++ task 5/inout.cpp
@@ -1,20 +1,20 @@
-#include "Header.h"
-
-#define MAX_VALUE_NUMBER 50
-
-void outArrayElements(int* array, int size);
-void randInit(int* array, int size);
-
-void randInit(int* array, int size) {
-	srand(time(NULL));
-
-	for (int i = 0; i < size; i++) {
-		array[i] = rand() % (MAX_VALUE_NUMBER + 1);
-	}
-}
-
-void outArrayElements(int* array, int size) {
-	for (int i = 0; i < size; i++) {
-		cout << array[i] << " ";
-	}
-}
+//#include "Header.h"
+//
+//#define MAX_VALUE_NUMBER 50
+//
+//void outArrayElements(int *array, int size);
+//void randInit(int *array, int size);
+//
+//void randInit(int *array, int size) {
+//	srand(time(NULL));
+//
+//	for (int i = 0; i < size; i++) {
+//		array[i] = rand() % (MAX_VALUE_NUMBER + 1);
+//	}
+//}
+//
+//void outArrayElements(int *array, int size) {
+//	for (int i = 0; i < size; i++) {
+//		cout << array[i] << " ";
+//	}
+//}
